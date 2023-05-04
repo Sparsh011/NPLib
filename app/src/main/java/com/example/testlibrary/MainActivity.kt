16f348a3735eea45758de85e6b0783711e8e4456
Sparsh <sparsh.chadha46@gmail.com>
2023-05-04 23:41:05 +0530

    library deployed

--- app/src/main/java/com/example/testlibrary/MainActivity.kt
+++ app/src/main/java/com/example/testlibrary/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.example.testlibrary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.test_library.NameAndPasswordView
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +10,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val nmp = NameAndPasswordView(this, null)
-        Toast.makeText(applicationContext, "${nmp.name} and ${nmp.password}", Toast.LENGTH_SHORT).show()
     }
 }
